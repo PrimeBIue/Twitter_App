48a74681ccaa84eb41d5e08591cdba4ddd37b5a7
Jorge Cabiedes <57368278+PrimeBIue@users.noreply.github.com>
2021-06-28 15:38:08 -0500

    Created Timeline Activity and Row layout

--- app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
@@ -1,16 +1,14 @@
-package com.example.twitter_app;
+package com.codepath.apps.restclienttemplate;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import com.codepath.apps.restclienttemplate.R;
-
-public class MainActivity extends AppCompatActivity {
+public class TimelineActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_timeline);
     }
 }
